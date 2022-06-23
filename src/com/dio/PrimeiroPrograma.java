dfb301ecefe1e5850f69e6ff55c261e95eaaf77a
caueenrico <caueenrico@gmail.com>
2022-06-22 21:42:40 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+	
+		
+		Gato gato = new Gato("Alfred", "preto", 25 );
+				
+		System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello Word " +  (a + b));*/
+	}
+
+}
